28801959829d4084f6e3a3efd7e042e494b843b6
Nathaly Elque <nathalyelque@gmail.com>
2020-04-21 19:15:30 -0400

    Aplicação SPA - páginas home e contato

---
+++ pizzariaAngularApp/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ContatoComponent } from './contato/contato.component';
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'contato', component: ContatoComponent }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
